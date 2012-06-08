de20168724bedc8077e15bcbc25ad2db726ac699
Uri Shaked <uri@salsa4fun.co.il>
2012-06-08 05:24:42 +0300

    Extract jar + sources & auto generate pom.xml file for artifacts

--- src/main/java/com/github/swt_release_fetcher/Artifact.java
+++ src/main/java/com/github/swt_release_fetcher/Artifact.java
@@ -3,13 +3,20 @@ package com.github.swt_release_fetcher;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.Writer;
 import java.net.URL;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
 
 import org.apache.commons.codec.digest.DigestUtils;
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
+import org.apache.velocity.VelocityContext;
+import org.apache.velocity.app.Velocity;
 
 public class Artifact {
 	private final File file;
@@ -51,11 +58,85 @@ public class Artifact {
 		System.out.println("DONE");
 	}
 
+	public void generatePom(File pomFile) throws IOException {
+		Velocity.addProperty("resource.loader", "class");
+		Velocity.addProperty("class.resource.loader.class",
+				"org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader");
+		Velocity.init();
+		VelocityContext context = new VelocityContext();
+
+		// TODO fill those correctly
+		context.put("version", "3.7.2");
+		context.put("artifactId", "foo.bar");
+
+		Writer writer = new FileWriter(pomFile);
+		try {
+			Velocity.mergeTemplate("pom.vm", "UTF-8", context, writer);
+		} finally {
+			writer.close();
+		}
+	}
+
+	public void deploy() throws IOException {
+		File jarFile = null;
+		File sourcesFile = null;
+		File pomFile = null;
+		ZipFile zipFile = null;
+
+		try {
+			jarFile = File.createTempFile("deploy", ".jar");
+			sourcesFile = File.createTempFile("deploy", "-sources.jar");
+			pomFile = File.createTempFile("pom", ".xml");
+
+			zipFile = new ZipFile(file);
+			extractFromZip(zipFile, "swt.jar", jarFile);
+			extractFromZip(zipFile, "src.zip", sourcesFile);
+			generatePom(pomFile);
+
+			runMavenDeploy(pomFile, jarFile, sourcesFile);
+
+		} finally {
+			if (zipFile != null) {
+				zipFile.close();
+			}
+			FileUtils.deleteQuietly(jarFile);
+			FileUtils.deleteQuietly(sourcesFile);
+			FileUtils.deleteQuietly(pomFile);
+		}
+	}
+
+	public void runMavenDeploy(File pomFile, File jarFile, File sourcesFile) {
+		// TODO
+		// mvn deploy:deploy-file -f %1.pom -DpomFile=%1.pom -Dfile=%1.jar
+		// -Durl=svn:https://swt-repo.googlecode.com/svn/repo
+		// -Dsources=%1-sources.jar
+	}
+
+	private void extractFromZip(ZipFile zipFile, String fileNameToExtract, File targetFile) throws IOException {
+		ZipEntry entry = zipFile.getEntry(fileNameToExtract);
+		InputStream inputStream = null;
+		OutputStream outputStream = null;
+		try {
+			inputStream = zipFile.getInputStream(entry);
+			outputStream = new FileOutputStream(targetFile);
+			IOUtils.copy(inputStream, outputStream);
+		} finally {
+			IOUtils.closeQuietly(inputStream);
+			IOUtils.closeQuietly(outputStream);
+		}
+	}
+
 	public File getFile() {
 		return file;
 	}
 
 	public boolean isNewDownload() {
 		return newDownload;
+	}
+
+	public static void main(String[] args) throws Exception {
+
+		Artifact art = new Artifact(new File("downloads/win32-win32-x86_64.zip"));
+		art.deploy();
 	}
 }
